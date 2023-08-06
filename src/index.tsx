4c9949dc1bc103f7f9e146c7d3b86b57160ac349
Alireza Safaierad <frontendmonster@gmail.com>
2023-08-06 14:07:41 +0400

    add react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,30 @@
 import './index.css';
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const router = createBrowserRouter([
+  {
+    path: '/overview',
+    element: <div>Overview</div>,
+  },
+  {
+    path: '/campaign',
+    element: <div>Campaign</div>,
+  },
+  {
+    path: '/*',
+    element: <Navigate to="/overview" />,
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
